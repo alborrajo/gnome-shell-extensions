0518366e19c05ae6669f912d05d5a9c2d6a5af86
alborrajo <alvarolopezborr@gmail.com>
2021-04-17 22:09:38 +0200

    places-menu: Icon selection through combobox

--- extensions/places-menu/prefs.js
+++ extensions/places-menu/prefs.js
@@ -1,7 +1,7 @@
 // -*- mode: js2; indent-tabs-mode: nil; js2-basic-offset: 4 -*-
 /* exported init buildPrefsWidget */
 
-const { Gio, GObject, Gtk } = imports.gi;
+const { Gio, GObject, Gtk, Gdk, GdkPixbuf } = imports.gi;
 
 const Gettext = imports.gettext.domain('gnome-shell-extensions');
 const _ = Gettext.gettext;
@@ -15,6 +15,7 @@ function init() {
 
 const WindowListPrefsWidget = GObject.registerClass(
 class WindowListPrefsWidget extends Gtk.Box {
+
     _init() {
         super._init({
             orientation: Gtk.Orientation.VERTICAL,
@@ -57,7 +58,42 @@ class WindowListPrefsWidget extends Gtk.Box {
             Gio.SettingsBindFlags.DEFAULT
         );
 
-        const iconEntryBuffer = new Gtk.EntryBuffer({
+        // Icon selector
+        const listStore = new Gtk.ListStore();
+        listStore.set_column_types([GObject.TYPE_STRING, GdkPixbuf.Pixbuf])
+
+        let cbox = new Gtk.ComboBox({model: listStore});
+        let rendererPixbuf = new Gtk.CellRendererPixbuf();
+        cbox.pack_start (rendererPixbuf, false);
+        cbox.add_attribute (rendererPixbuf, "icon-name", 0);
+        cbox.set_active(0);
+        let rendererText = new Gtk.CellRendererText();
+        cbox.pack_start (rendererText, false);
+        cbox.add_attribute (rendererText, "text", 0);
+        cbox.set_active(1);
+
+        iconBox.append(cbox);
+
+        const selectedIconName = this._settings.get_string('icon-path')
+        const iconNames = ["folder-open", "compass", "folder"];
+        const gabo = Gtk.IconTheme.get_for_display(Gdk.Display.get_default()).get_icon_names();
+        for(const iconName of iconNames) {
+            const iter = listStore.append();
+            listStore.set(iter, [0], [iconName]);
+
+            if(iconName === selectedIconName) {
+                cbox.set_active_iter(iter);
+            }
+        }
+
+        // Save settings when selecting an item
+        cbox.connect ('changed', (entry) => {
+            const [success, selectedIter] = cbox.get_active_iter()
+            if(success)
+                this._settings.set_string('icon-path', listStore.get_value(selectedIter, 0));
+        });
+        
+        /*const iconEntryBuffer = new Gtk.EntryBuffer({
             text: this._settings.get_string('icon-path')
         })
         const iconEntry = new Gtk.Entry({
@@ -70,7 +106,7 @@ class WindowListPrefsWidget extends Gtk.Box {
             iconEntryBuffer,
             'text',
             Gio.SettingsBindFlags.DEFAULT
-        );
+        );*/
 
         /*const iconButton = new Gtk.Button()
         iconButton.set_child(new Gtk.Image({
